8b20e5b892e2f31bdcbcab3c52c752ee8dca5037
tll-aww-app <owner@SSE-00-00-MBP-O-3.local>
2015-04-10 01:13:00 -0500

    Finished Room Types&Check-in Policies

---
+++ PBR_Directory/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  PBR_Directory
+//
+//  Created by Owner on 3/29/15.
+//  Copyright (c) 2015 AlexWatts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIScrollViewDelegate {
+
+    @IBOutlet weak var HomePageScrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //HomePageScrollView Stuff
+        let image1 = UIImage(named: "BeachKids1.jpg")
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    
+}
+
